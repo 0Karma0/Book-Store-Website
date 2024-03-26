e194e1b116122fddfe98c2b8d7fd0b07594aa2c3
mahade <mahade.adib45@gmail.com>
2024-03-26 22:29:43 +0600

    edited

--- src/Hooks/BooksReviewData.jsx
+++ src/Hooks/BooksReviewData.jsx
@@ -3,20 +3,17 @@ import { useEffect, useState } from 'react';
 const BooksReviewData = () => {
 
     const [data, setData] = useState([])
-    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
         const fetchData = async () =>{
-            setLoading(true);
             const res = await fetch('/public/data.json')
             const data = await res.json();
             setData(data);
-            setLoading(false);
         } 
 
         fetchData();
     }, []);
-    return {data, loading};
+    return {data};
 };
 
 export default BooksReviewData;
